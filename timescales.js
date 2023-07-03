2fe6ced81da9b7bebe72f1b7e933af6ae280554a
hongfaqiu <951142905@qq.com>
2023-07-03 16:29:28 +0800

    chore: demo version change

---
+++ timescales.js
@@ -0,0 +1,117 @@
+const TimeScaleVersions = [
+  {
+    name: 'GTS2020',
+    desc: 'GTS2020 means"A geologic time scale 2020",which has 1380 triples.Covers the geological duration from 0 to 542 Ma. Includes 153 Geochronologic era in total.\nCited from(Gradstein, F.M., Ogg, J.G., Schmitz, M.D., & Ogg, G.M. (Eds.). (2020).Geologic time scale 2020. Elsevier.) ',
+    image: 'https://resource.deep-time.org/resource/mapViewPic/ICS2020.png',
+    data: "./assets/GTS_2020.json"
+  },
+  {
+    name: 'GTS2012',
+    desc: 'GTS2012 means " The geologic time scale 2012", which has 1371 triples. Covers the geological duration from 0 to 541 Ma. Includes 152 Geochronologic era in total.\nCited from(Gradstein, F.M., Ogg, J.G., Schmitz, M.B., & Ogg, G.M. (Eds.). (2012).The geologic time scale 2012. elsevier.) ',
+    image: 'https://resource.deep-time.org/resource/mapViewPic/ICS2012.png',
+    data: "./assets/GTS_2012.json"
+  },
+  {
+    "name": "ICS200604",
+    "desc": "ICS200604 ICS200604 means 'International Chronostratigraphic Chart (2007)', which has 1525 triples. Covers the geological duration from 0 to 3600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2007 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS200604/31_ICS200604&200604.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS200604/ICS200604.jpg"
+  }, {
+    "name": "ICS200808",
+    "desc": "ICS200808 ICS200808 means 'International Chronostratigraphic Chart (2008)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2008 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS200808/32_ICS200808&200808.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS200808/ICS200808_1.jpg"
+  }, {
+    "name": "ICS200908",
+    "desc": "ICS200908 ICS200908 means 'International Chronostratigraphic Chart (2009)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2009 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS200908/33_ICS200908&200908.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS200908/ICS200908_1.jpg"
+  }, {
+    "name": "ICS201009",
+    "desc": "ICS201009 ICS201009 means 'International Chronostratigraphic Chart (2010)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2010 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201009/34_ICS201009&201009.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201009/ICS201009_1.jpg"
+  }, {
+    "name": "ICS201208",
+    "desc": "ICS200812 ICS200808 means 'International Chronostratigraphic Chart (2008)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2012 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201208/35_ICS201208&201208.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201208/ICS201208_1.jpg"
+  }, {
+    "name": "ICS201301",
+    "desc": "ICS201301 ICS201301 means 'International Chronostratigraphic Chart (2008)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2013 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201301/37_ICS201301&201301.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201301/ICS201301_1.jpg"
+  }, {
+    "name": "ICS201402",
+    "desc": "ICS201402 ICS201402 means 'International Chronostratigraphic Chart (2008)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2014 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201402/38_ICS201402&201402.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201402/ICS201402_1.jpg"
+  }, {
+    "name": "ICS201410",
+    "desc": "ICS201410 ICS201410 means 'International Chronostratigraphic Chart (2014)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2014 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201410/39_ICS201410&201410.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201410/ICS201410_1.jpg"
+  }, {
+    "name": "ICS201501",
+    "desc": "ICS201501 ICS201501 means 'International Chronostratigraphic Chart (2015)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2015 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201501/40_ICS201501&201501.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201501/ICS201501_1.jpg"
+  }, {
+    "name": "ICS201604",
+    "desc": "ICS201604 ICS200808 means 'International Chronostratigraphic Chart (2016/04)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a92016 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201604/41_ICS201604&201604.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201604/ICS201604_1.jpg"
+  }, {
+    "name": "ICS201610",
+    "desc": "ICS201610 ICS200808 means 'International Chronostratigraphic Chart (2016/10)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2016 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201610/43_ICS201610&201610.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201610/ICS201610_1.jpg"
+  }, {
+    "name": "ICS201702",
+    "desc": "ICS201702 ICS201702 means 'International Chronostratigraphic Chart (2017/02)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2017 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201702/44_ICS201702&201702.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201702/ICS201702_1.jpg"
+  }, {
+    "name": "ICS201807",
+    "desc": "ICS201807 ICS201807 means 'International Chronostratigraphic Chart (2018/07)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2018 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201807/45_ICS201807&201807.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201807/ICS201807_1.jpg"
+  }, {
+    "name": "ICS201808",
+    "desc": "ICS201808 ICS201808 means 'International Chronostratigraphic Chart (2018/08)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2018 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201808/46_ICS201808&201808.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201808/ICS201808_1.jpg"
+  }, {
+    "name": "ICS201905",
+    "desc": "ICS201905 ICS201905means 'International Chronostratigraphic Chart (2019/05)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2019 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS201905/47_ICS201905&201905.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS201905/ICS201905_1.jpg"
+  }, {
+    "name": "ICS202001",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS202001/48_ICS202001&ICS202001.json",
+    "desc": "ICS202001 ICS202001 means 'International Chronostratigraphic Chart (2020)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2020 International Commission on Stratigraphy)",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS202001/ICS202001_1.jpg"
+  }, {
+    "name": "ICS202003",
+    "desc": "ICS202003 ICS202003 means 'International Chronostratigraphic Chart (2020)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2020 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS202003/49_ICS202003&202003.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS202003/ICS202003_1.jpg"
+  }, {
+    "name": "ICS202105",
+    "desc": "ICS202105 ICS202105 means 'International Chronostratigraphic Chart (2021/05)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2021 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS202105/51_ICS202105&202105.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS202105/ICS202105_1.jpg"
+  }, {
+    "name": "ICS202107",
+    "desc": "ICS202107 ICS202107 means 'International Chronostratigraphic Chart (2021/07)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2021 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS202107/52_ICS202107&202107.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS202107/ICS202107_1.jpg"
+  }, {
+    "name": "ICS202202",
+    "desc": "ICS202202 ICS202202 means 'International Chronostratigraphic Chart (2022/02)', which has 1624 triples. Covers the geological duration from 0 to 4600 Ma. \nCited from (International Commission on Stratigraphy/Original version Copyright \u00a9 2022 International Commission on Stratigraphy)",
+    "data": "https://resource.deep-time.org/resource/timescale/ICS202202/53_ICS202202&202202.json",
+    "image": "https://resource.deep-time.org/resource/timescale/ICS202202/ICS202202_1.jpg"
+  }
+]
+
+export default TimeScaleVersions
